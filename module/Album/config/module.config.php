18be8185a3bc2ed5c335d6d4bfc4a5da4beb1003
williamdelrosario <williamdelrosario@yahoo.com>
2017-12-13 17:22:50 +0100

    Fixed Routing and Controllers.

--- module/Album/config/module.config.php
+++ module/Album/config/module.config.php
@@ -2,6 +2,7 @@
 
 namespace Album;
 
+use Zend\Router\Http\Segment;
 use Zend\ServiceManager\Factory\InvokableFactory;
 
 return [
@@ -10,6 +11,27 @@ return [
             Controller\AlbumController::class => InvokableFactory::class,
         ],
     ],
+
+    // The following section is new and should be added to your file:
+    'router' => [
+        'routes' => [
+            'album' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route' => '/album[/:action[/:id]]',
+                    'constraints' => [
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ],
+                    'defaults' => [
+                        'controller' => Controller\AlbumController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
+
     'view_manager' => [
         'template_path_stack' => [
             'album' => __DIR__ . '/../view',
