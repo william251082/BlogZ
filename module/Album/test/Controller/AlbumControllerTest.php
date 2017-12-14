19b58a90275ef635f57fbb51b9f3abbabccdb5f0
williamdelrosario <williamdelrosario@yahoo.com>
2017-12-14 11:10:54 +0100

    First test, success.

---
+++ module/Album/test/Controller/AlbumControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+namespace AlbumTest\Controller;
+
+use Album\Controller\AlbumController;
+use Zend\Stdlib\ArrayUtils;
+use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+
+class AlbumControllerTest extends AbstractHttpControllerTestCase
+{
+    protected $traceError = false;
+
+    public function setUp()
+    {
+        // The module configuration should still be applicable for tests.
+        // You can override configuration here with test case specific values,
+        // such as sample view templates, path stacks, module_listener_options,
+        // etc.
+        $configOverrides = [];
+
+        $this->setApplicationConfig(ArrayUtils::merge(
+            // Grabbing the full application configuration:
+            include __DIR__ . '/../../../../config/application.config.php',
+            $configOverrides
+        ));
+        parent::setUp();
+    }
+}
