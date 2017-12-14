784ea4a3446d501d3911bdbec3d1654de2ca86b6
williamdelrosario <williamdelrosario@yahoo.com>
2017-12-14 18:57:50 +0100

    Refactoring hidden dependencies and polished the repository.

--- module/Blog/src/Factory/ZendDbSqlRepositoryFactory.php
+++ module/Blog/src/Factory/ZendDbSqlRepositoryFactory.php
@@ -3,20 +3,20 @@
 namespace Blog\Factory;
 
 use Interop\Container\ContainerInterface;
+use Blog\Model\Post;
 use Blog\Model\ZendDbSqlRepository;
 use Zend\Db\Adapter\AdapterInterface;
+use Zend\Hydrator\Reflection as ReflectionHydrator;
 use Zend\ServiceManager\Factory\FactoryInterface;
 
 class ZendDbSqlRepositoryFactory implements FactoryInterface
 {
-    /**
-     * @param ContainerInterface $container
-     * @param string $requestedName
-     * @param null|array $options
-     * @return ZendDbSqlRepository
-     */
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
-        return new ZendDbSqlRepository($container->get(AdapterInterface::class));
+        return new ZendDbSqlRepository(
+            $container->get(AdapterInterface::class),
+            new ReflectionHydrator(),
+            new Post('', '')
+        );
     }
 }
--- module/Blog/src/Model/ZendDbSqlRepository.php
+++ module/Blog/src/Model/ZendDbSqlRepository.php
@@ -4,12 +4,11 @@ namespace Blog\Model;
 
 use InvalidArgumentException;
 use RuntimeException;
+use Zend\Hydrator\HydratorInterface;
 use Zend\Db\Adapter\AdapterInterface;
-use Zend\Db\Sql\Sql;
 use Zend\Db\Adapter\Driver\ResultInterface;
-use Zend\Hydrator\Reflection as ReflectionHydrator;
 use Zend\Db\ResultSet\HydratingResultSet;
-
+use Zend\Db\Sql\Sql;
 //use Zend\Db\ResultSet\ResultSet;
 
 class ZendDbSqlRepository implements PostRepositoryInterface
@@ -20,41 +19,82 @@ class ZendDbSqlRepository implements PostRepositoryInterface
     private $db;
 
     /**
-     * @param AdapterInterface $db
+     * @var HydratorInterface
      */
-    public function __construct(AdapterInterface $db)
-    {
-        $this->db = $db;
+    private $hydrator;
+
+    /**
+     * @var Post
+     */
+    private $postPrototype;
+
+    public function __construct(
+        AdapterInterface $db,
+        HydratorInterface $hydrator,
+        Post $postPrototype
+    ) {
+        $this->db            = $db;
+        $this->hydrator      = $hydrator;
+        $this->postPrototype = $postPrototype;
     }
 
     /**
-     * {@inheritDoc}
+     * Return a set of all blog posts that we can iterate over.
+     *
+     * Each entry should be a Post instance.
+     *
+     * @return Post[]
      */
     public function findAllPosts()
+    {
+        $sql       = new Sql($this->db);
+        $select    = $sql->select('posts');
+        $statement = $sql->prepareStatementForSqlObject($select);
+        $result    = $statement->execute();
+
+        if (! $result instanceof ResultInterface || ! $result->isQueryResult()) {
+            return [];
+        }
+
+        $resultSet = new HydratingResultSet($this->hydrator, $this->postPrototype);
+        $resultSet->initialize($result);
+        return $resultSet;
+    }
+
+    /**
+     * Return a single blog post.
+     *
+     * @param  int $id Identifier of the post to return.
+     * @return Post
+     */
+    public function findPost($id)
 {
     $sql       = new Sql($this->db);
     $select    = $sql->select('posts');
+    $select->where(['id = ?' => $id]);
+
     $statement = $sql->prepareStatementForSqlObject($select);
     $result    = $statement->execute();
 
     if (! $result instanceof ResultInterface || ! $result->isQueryResult()) {
-        return [];
+        throw new RuntimeException(sprintf(
+            'Failed retrieving blog post with identifier "%s"; unknown database error.',
+            $id
+        ));
     }
 
-    $resultSet = new HydratingResultSet(
-        new ReflectionHydrator(),
-        new Post('', '')
-    );
+    $resultSet = new HydratingResultSet($this->hydrator, $this->postPrototype);
     $resultSet->initialize($result);
-    return $resultSet;
-}
+    $post = $resultSet->current();
 
-    /**
-     * {@inheritDoc}
-     * @throws InvalidArgumentException
-     * @throws RuntimeException
-     */
-    public function findPost($id)
-    {
+    if (! $post) {
+        throw new InvalidArgumentException(sprintf(
+            'Blog post with identifier "%s" not found.',
+            $id
+        ));
     }
+
+    return $post;
+	}
+
 }
